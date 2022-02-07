54c8d98559332f1b0349f33839407a09c1f75926
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2022-02-07 15:28:36 +0200

    fix quotes

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts"
 
-console.log("Listening on http://localhost:8000")
+console.log(`Listening on http://localhost:8000`)
 
 serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
+  return new Response(`Hello World!`, {
+    headers: { 'content-type': `text/plain` },
   });
 })
+
