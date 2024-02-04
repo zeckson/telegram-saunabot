070f50f4fa028ef03fec8a4d6af06f014627c337
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-02-04 21:18:27 +0530

    fix deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
 export { Bot,Context, webhookCallback } from "https://deno.land/x/grammy@v1.7.0/mod.ts";
 export type { NextFunction } from "https://deno.land/x/grammy@v1.7.0/mod.ts";
 export { Application, Router } from "https://deno.land/x/oak@v10.2.0/mod.ts";
-export { config } from "https://deno.land/x/dotenv/mod.ts";
+export { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
