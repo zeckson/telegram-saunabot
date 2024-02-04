63064d550655447a75535ca126e271b98213001f
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-02-04 22:04:13 +0530

    refactor bot split server/local

--- main.ts
+++ main.ts
@@ -1,22 +1,6 @@
 import { bot } from "./src/bot.ts";
-import { setupWebhook } from "./src/webhook.ts";
-
-const projectId = Deno.env.get(`DENO_PROJECT_ID`) || `telegram-saunabot`;
-const deploymentId = Deno.env.get(`DENO_DEPLOYMENT_ID`);
-const deployUrl = deploymentId ?
-  `https://${projectId}${deploymentId ? `-${deploymentId}` : ``}.deno.dev` :
-  `http://localhost:8000`;
-
 
 await bot.init();
-
-if (!deploymentId) {
-  bot.start();
-} else {
-  await setupWebhook(bot);
-  console.log(
-    `Deno deploy url: ${deployUrl}`,
-  );
-}
+bot.start();
 
 console.log(`Bot has been started: https://t.me/${bot.botInfo.username}`);
