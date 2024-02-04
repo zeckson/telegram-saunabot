7b97dd681e186bf9ed2894ca034b2e4d60149602
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-02-04 22:30:46 +0530

    add more logging

--- server.ts
+++ server.ts
@@ -20,6 +20,7 @@ Deno.serve(async (req) => {
   if (req.method == "POST") {
     const url = new URL(req.url);
     if (url.pathname.slice(1) == bot.token) {
+      console.log("Got webhook request")
       try {
         response = await handleUpdate(req);
       } catch (err) {
