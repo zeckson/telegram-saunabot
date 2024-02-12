6857bfbe028714acec0201f5880f71125fe01c1c
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-02-12 20:49:21 +0530

    fix logs

--- src/middleware/log.ts
+++ src/middleware/log.ts
@@ -2,18 +2,24 @@ import { Context, NextFunction } from '../deps.ts'
 
 export const log = async (ctx: Context, next: NextFunction) => {
   const from = ctx.from || { username: `unknown`, id: `unknown` }
+  const messageId = `(id:${ctx.msg?.message_id})`
+  console.log(`Got message ${messageId} from: @${from.username}[${from.id}]`)
+  // take time before
+  const before = Date.now() // milliseconds
 
   console.log(`Message:`)
   console.dir(ctx.msg)
   console.log(`======`)
 
-  const messageId = `(id:${ctx.msg?.message_id})`
-  console.log(`Got message ${messageId} from: @${from.username}[${from.id}]`)
-  // take time before
-  const before = Date.now() // milliseconds
   // invoke downstream middleware
-  await next() // make sure to `await`!
-  // take time after
-  // log difference
-  console.log(`Response time ${messageId}: ${Date.now() - before} ms`)
+  try {
+    await next() // make sure to `await`!
+  } catch (e) {
+    console.error(`Got error: ${e}`)
+  } finally {
+    // take time after
+    // log difference
+    console.log(`Response time ${messageId}: ${Date.now() - before} ms`)
+  }
+
 }
