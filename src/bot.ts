1030bc4e9d45bae4c34a02b6e71dfa53c6a55d41
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-02-13 21:38:45 +0530

    add i18n implementation

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,5 @@
 import { GroupContext } from "./context.ts"
-import { Bot } from './deps.ts'
+import { Bot, I18n, I18nFlavor } from './deps.ts'
 import { requireEnv } from './env-util.ts'
 import { isInChat } from './middleware/guard.ts'
 import { log } from './middleware/log.ts'
@@ -7,16 +7,39 @@ import { log } from './middleware/log.ts'
 const TELEGRAM_TOKEN = requireEnv(`TELEGRAM_TOKEN`, true)
 const SAUNA_CHAT_ID_NAME = requireEnv(`SAUNA_CHAT_ID`)
 
+type BotContext = GroupContext & I18nFlavor;
 // Create bot object
-const bot = new Bot<GroupContext>(TELEGRAM_TOKEN)
+const bot = new Bot<BotContext>(TELEGRAM_TOKEN)
 
 bot.use(log)
 const isInSaunaChat = isInChat(SAUNA_CHAT_ID_NAME)
 bot.use(isInSaunaChat)
 
+// For TypeScript and auto-completion support,
+// extend the context with I18n's flavor:
+
+// Create an `I18n` instance.
+// Continue reading to find out how to configure the instance.
+const i18n = new I18n<BotContext>({
+    defaultLocale: `ru`,
+    localeNegotiator: () => `ru`, // Default everything to ru
+    globalTranslationContext(ctx) {
+        return {
+            username: ctx.user.fullName,
+        };
+    },
+});
+
+// Translation files loaded this way works in Deno Deploy, too.
+await i18n.loadLocalesDir("locales");
+
+// Finally, register the i18n instance in the bot,
+// so the messages get translated on their way!
+bot.use(i18n);
+
 // Listen for messages
-bot.command(`start`, (ctx) => {
-    return ctx.reply(`Welcome! Send me a photo!`)
+bot.command(`start`, (ctx: BotContext) => {
+    return ctx.reply(ctx.t(`greeting`))
 })
 
 bot.on(`message:text`, (ctx) => ctx.reply(`That is text and not a photo!`))
--- src/deps.ts
+++ src/deps.ts
@@ -2,6 +2,8 @@ export {
   Bot,
   Context,
   webhookCallback,
-} from 'https://deno.land/x/grammy@v1.20.3/mod.ts'
-export type { NextFunction } from 'https://deno.land/x/grammy@v1.20.3/mod.ts'
+} from 'https://deno.land/x/grammy@v1.20.4/mod.ts'
+export type { NextFunction } from 'https://deno.land/x/grammy@v1.20.4/mod.ts'
+export { I18n } from "https://deno.land/x/grammy_i18n@v1.0.1/mod.ts";
+export type { I18nFlavor } from "https://deno.land/x/grammy_i18n@v1.0.1/mod.ts";
 import 'https://deno.land/std@0.214.0/dotenv/load.ts'
