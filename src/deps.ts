a826b54c33733463a3a7a044e6b293354532ff18
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-06-12 19:20:29 +0700

    update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -3,8 +3,8 @@ export {
   Context,
   webhookCallback,
   InlineKeyboard
-} from 'https://deno.land/x/grammy@v1.20.4/mod.ts'
-export type { NextFunction } from 'https://deno.land/x/grammy@v1.20.4/mod.ts'
-export { I18n } from "https://deno.land/x/grammy_i18n@v1.0.1/mod.ts";
-export type { I18nFlavor } from "https://deno.land/x/grammy_i18n@v1.0.1/mod.ts";
+} from 'https://deno.land/x/grammy@v1.24.1/mod.ts'
+export type { NextFunction } from 'https://deno.land/x/grammy@v1.24.1/mod.ts'
+export { I18n } from "https://deno.land/x/grammy_i18n@v1.0.2/mod.ts";
+export type { I18nFlavor } from "https://deno.land/x/grammy_i18n@v1.0.2/mod.ts";
 import 'https://deno.land/std@0.214.0/dotenv/load.ts'
