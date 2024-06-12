ba33c13c7bc3dec670ff37f8e195a8397edabe36
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-06-12 16:51:21 +0700

    add buttons

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,5 @@
 import { UserContext } from './context.ts'
-import { Bot, I18nFlavor } from './deps.ts'
+import { Bot, I18nFlavor, InlineKeyboard } from './deps.ts'
 import { requireEnv } from './env-util.ts'
 import { log } from './middleware/log.ts'
 import { getUsername } from "./util/username.ts"
@@ -16,9 +16,47 @@ bot.use(log)
 bot.on(`chat_join_request`, async (ctx) => {
   const chat = ctx.chat;
   const from = ctx.from;
+
+  const keyboard = [[
+    InlineKeyboard.text(`Подтвердить`, `approve:${chat.id}:${from.id}`),
+    InlineKeyboard.text(`Отклонить`, `reject:${chat.id}:${from.id}`)
+  ]]
+
   await bot.api.sendMessage(ADMIN_ID, `Новая заявка на добавление от ${getUsername(from)} в чат "${chat.title}"
 Проверить пользователя можно по сыылке:
-https://t.me/lolsbotcatcherbot?start=${from.id}`, { link_preview_options: { is_disabled: true } })
+https://t.me/lolsbotcatcherbot?start=${from.id}`, {
+    link_preview_options: { is_disabled: true },
+    reply_markup: new InlineKeyboard(keyboard)
+  })
+})
+
+const handleQuery = async (ctx: BotContext) => {
+  const data = ctx.callbackQuery?.data ?? ``
+  const [action, chatId, userId] = data.split(`:`)
+
+  let result = `Неизвестная команда`
+  switch (action) {
+    case `approve`:
+      bot.api.approveChatJoinRequest(chatId, parseInt(userId, 10)).catch((e) => console.error(`Approve failed: ${e}`))
+      result = `Добавлен в группу`
+      break
+    case `reject`:
+      bot.api.declineChatJoinRequest(chatId, parseInt(userId, 10)).catch((e) => console.error(`Decline failed: ${e}`))
+      result = `Отклонён`
+      break
+    default:
+      console.error(`Unknown action: ${action}`)
+  }
+  return result
+}
+
+// TODO: Prevent access from other
+bot.on(`callback_query:data`, async (ctx) => {
+  const result = await handleQuery(ctx)
+
+  await ctx.answerCallbackQuery(result)
+
+  await ctx.deleteMessage()
 })
 
 bot.on(`my_chat_member`, (ctx) => {
--- src/deps.ts
+++ src/deps.ts
@@ -2,6 +2,7 @@ export {
   Bot,
   Context,
   webhookCallback,
+  InlineKeyboard
 } from 'https://deno.land/x/grammy@v1.20.4/mod.ts'
 export type { NextFunction } from 'https://deno.land/x/grammy@v1.20.4/mod.ts'
 export { I18n } from "https://deno.land/x/grammy_i18n@v1.0.1/mod.ts";
