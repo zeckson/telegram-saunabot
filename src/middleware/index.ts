f2f24c31ba8de52de43ad9b55e05ebdc23da220a
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-07-11 13:32:50 +0300

    add replyMiddleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,9 +1,15 @@
-import { Bot } from '../deps.ts'
+import { Bot, hydrateReply } from '../deps.ts'
 import { BotContext } from '../type/context.ts'
 import { log } from './log.ts'
 import { registerTranslate } from './translate.ts'
 
 export const registerMiddleware = async (bot: Bot<BotContext>) => {
+  // Install logger
   bot.use(log)
+
+  // Install format reply variant to ctx
+  bot.use(hydrateReply)
+
+  // Set translation
   await registerTranslate(bot)
 }
