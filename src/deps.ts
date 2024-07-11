deed3524cc4d87278cb30a4547c14788c28dcb0f
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-07-11 13:24:37 +0300

    add parseMode dependency

--- src/deps.ts
+++ src/deps.ts
@@ -7,6 +7,14 @@ export {
 } from 'https://deno.land/x/grammy@v1.24.1/mod.ts'
 export type { NextFunction } from 'https://deno.land/x/grammy@v1.24.1/mod.ts'
 export type { User, ChatJoinRequest } from 'https://deno.land/x/grammy@v1.24.1/types.ts'
+
 export { I18n } from 'https://deno.land/x/grammy_i18n@v1.0.2/mod.ts'
 export type { I18nFlavor } from 'https://deno.land/x/grammy_i18n@v1.0.2/mod.ts'
+
+export {
+  hydrateReply,
+  parseMode,
+} from "https://deno.land/x/grammy_parse_mode@1.10.0/mod.ts";
+export type { ParseModeFlavor } from "https://deno.land/x/grammy_parse_mode@1.10.0/mod.ts";
+
 import 'https://deno.land/std@0.214.0/dotenv/load.ts'
