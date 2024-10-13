accd50a497830dc1042ec0f9c78a6f392eee0798
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-10-13 09:09:12 +0300

    split action and handler

---
+++ src/action/admin.ts
@@ -0,0 +1,104 @@
+import { ChatJoinRequest, InlineKeyboard } from '../deps.ts'
+import { BotContext } from "../type/context.ts"
+import { escapeSpecial, link, tgIdLink } from "../util/string.ts"
+import { int } from "../util/system.ts"
+import { getUserLink } from "../util/username.ts"
+import { notifyAdmins } from "./notify-admin.ts"
+
+const enum JoinRequestAction {
+  APPROVE = `approve`,
+  DECLINE = `decline`,
+}
+
+const notifyAllAdmins = (ctx: BotContext, message: string, other?: object) =>
+  notifyAdmins((id: number) => ctx.api.sendMessage(id, message, {...other, parse_mode: "MarkdownV2"}))
+
+export const notifyAdminsOnJoinRequest = (ctx: BotContext & ChatJoinRequest) => {
+  const chat = ctx.chat
+  const from = ctx.from
+  const updateId = ctx.update.update_id
+
+  const keyboard = [[
+    InlineKeyboard.text(
+      ctx.t(`chat-join-request_approve`),
+      `${JoinRequestAction.APPROVE}:${chat.id}:${from.id}:${updateId}`,
+    ),
+    InlineKeyboard.text(
+      ctx.t(`chat-join-request_decline`),
+      `${JoinRequestAction.DECLINE}:${chat.id}:${from.id}:${updateId}`,
+    ),
+  ]]
+
+  const title = chat.title ?? chat.type
+  const safeChatTitle = escapeSpecial(title)
+  const vars = {
+    id: String(updateId),
+    userLink: getUserLink(from),
+    chatLink: chat.username
+      ? `[${safeChatTitle}](tg://resolve?domain=${chat.username})`
+      : safeChatTitle,
+    verifyLink: `[ссылке](https://t.me/lolsbotcatcherbot?start=${from.id})`,
+  }
+
+  // NB!: grammyjs breaks message inserting invalid chars inside interpolation "{ $variable }"
+  // NB!: grammyjs automatically formats numbers which breaks links to ids
+  const message = ctx.t(`chat-join-request_admin-notify-text`, vars)
+
+  return notifyAllAdmins(ctx, message, {
+    link_preview_options: { is_disabled: true },
+    reply_markup: new InlineKeyboard(keyboard),
+  })
+}
+
+const notifyApproved = (ctx: BotContext, updateId: string) => () =>
+  notifyAllAdmins(
+    ctx,
+    ctx.t(`chat-join-request_admin-approve-text`, {
+      id: updateId,
+      adminLink: link(`админ`, tgIdLink(ctx.from!.id)),
+    }),
+  )
+
+const notifyRejected = (ctx: BotContext, updateId: string) => () =>
+  notifyAllAdmins(
+    ctx,
+    ctx.t(`chat-join-request_admin-reject-text`, {
+      id: updateId,
+      adminLink: link(`админ`, tgIdLink(ctx.from!.id)),
+    }),
+  )
+
+const notifyErrored = (ctx: BotContext, updateId: string) => (e: Error) => {
+  console.error(`Got error: `, e)
+  return notifyAllAdmins(
+    ctx,
+    ctx.t(`chat-join-request_admin-error-text`, {
+      id: updateId,
+      adminLink: link(`админ`, tgIdLink(ctx.from!.id)),
+      errorText: escapeSpecial(e.message),
+    })
+  ).catch((err) => console.error(`Failed to notify: `, err))
+}
+
+export const handleJoinAction = (ctx: BotContext) => {
+  const data = ctx.callbackQuery?.data ?? ``
+  const [action, chatId, userId, updateId] = data.split(`:`)
+
+  let result = ctx.t(`chat-join-request_unknown-command`)
+  switch (action) {
+    case JoinRequestAction.APPROVE:
+      ctx.api.approveChatJoinRequest(chatId, int(userId))
+        .then(notifyApproved(ctx, updateId)).catch(notifyErrored(ctx, updateId))
+      result = ctx.t(`chat-join-request_added-to-group`)
+      break
+    case JoinRequestAction.DECLINE:
+      ctx.api.declineChatJoinRequest(chatId, int(userId))
+        .then(notifyRejected(ctx, updateId)).catch(notifyErrored(ctx, updateId))
+      result = ctx.t(`chat-join-request_declined-to-group`)
+      break
+    default:
+      console.error(`Unknown action: ${action}`)
+  }
+  return result
+}
+
--- src/handler/chat-join-request.ts
+++ src/handler/chat-join-request.ts
@@ -1,106 +1,13 @@
-import { notifyAdmins } from "../action/notify-admin.ts"
-import { Bot, ChatJoinRequest, InlineKeyboard } from '../deps.ts'
+import {
+  handleJoinAction,
+  notifyAdminsOnJoinRequest,
+} from "../action/admin.ts"
+import { Bot } from '../deps.ts'
 import { BotContext } from '../type/context.ts'
-import { escapeSpecial, link, tgIdLink } from "../util/string.ts"
-import { int } from '../util/system.ts'
-import { getUserLink } from "../util/username.ts"
 
-export const enum JoinRequestAction {
-  APPROVE = `approve`,
-  DECLINE = `decline`,
-}
+export const notifyJoinRequest = notifyAdminsOnJoinRequest
 
-const notifyAll = (ctx: BotContext, message: string, other?: object) =>
-  notifyAdmins((id: number) => ctx.api.sendMessage(id, message, {...other, parse_mode: "MarkdownV2"}))
-
-export const notifyJoinRequest = (ctx: BotContext & ChatJoinRequest) => {
-  const chat = ctx.chat
-  const from = ctx.from
-  const updateId = ctx.update.update_id
-
-  const keyboard = [[
-    InlineKeyboard.text(
-      ctx.t(`chat-join-request_approve`),
-      `${JoinRequestAction.APPROVE}:${chat.id}:${from.id}:${updateId}`,
-    ),
-    InlineKeyboard.text(
-      ctx.t(`chat-join-request_decline`),
-      `${JoinRequestAction.DECLINE}:${chat.id}:${from.id}:${updateId}`,
-    ),
-  ]]
-
-  const title = chat.title ?? chat.type
-  const safeChatTitle = escapeSpecial(title)
-  const vars = {
-    id: String(updateId),
-    userLink: getUserLink(from),
-    chatLink: chat.username
-      ? `[${safeChatTitle}](tg://resolve?domain=${chat.username})`
-      : safeChatTitle,
-    verifyLink: `[ссылке](https://t.me/lolsbotcatcherbot?start=${from.id})`,
-  }
-
-  // NB!: grammyjs breaks message inserting invalid chars inside interpolation "{ $variable }"
-  // NB!: grammyjs automatically formats numbers which breaks links to ids
-  const message = ctx.t(`chat-join-request_admin-notify-text`, vars)
-
-  return notifyAll(ctx, message, {
-    link_preview_options: { is_disabled: true },
-    reply_markup: new InlineKeyboard(keyboard),
-  })
-}
-
-const approved = (ctx: BotContext, updateId: string) => () =>
-  notifyAll(
-    ctx,
-    ctx.t(`chat-join-request_admin-approve-text`, {
-      id: updateId,
-      adminLink: link(`админ`, tgIdLink(ctx.from!.id)),
-    }),
-  )
-
-const rejected = (ctx: BotContext, updateId: string) => () =>
-  notifyAll(
-    ctx,
-    ctx.t(`chat-join-request_admin-reject-text`, {
-      id: updateId,
-      adminLink: link(`админ`, tgIdLink(ctx.from!.id)),
-    }),
-  )
-
-const errored = (ctx: BotContext, updateId: string) => (e: Error) => {
-  console.error(`Got error: `, e)
-  return notifyAll(
-    ctx,
-    ctx.t(`chat-join-request_admin-error-text`, {
-      id: updateId,
-      adminLink: link(`админ`, tgIdLink(ctx.from!.id)),
-      errorText: escapeSpecial(e.message),
-    })
-  ).catch((err) => console.error(`Failed to notify: `, err))
-}
-
-const handleQuery = (ctx: BotContext) => {
-  const data = ctx.callbackQuery?.data ?? ``
-  const [action, chatId, userId, updateId] = data.split(`:`)
-
-  let result = ctx.t(`chat-join-request_unknown-command`)
-  switch (action) {
-    case JoinRequestAction.APPROVE:
-      ctx.api.approveChatJoinRequest(chatId, int(userId))
-        .then(approved(ctx, updateId)).catch(errored(ctx, updateId))
-      result = ctx.t(`chat-join-request_added-to-group`)
-      break
-    case JoinRequestAction.DECLINE:
-      ctx.api.declineChatJoinRequest(chatId, int(userId))
-        .then(rejected(ctx, updateId)).catch(errored(ctx, updateId))
-      result = ctx.t(`chat-join-request_declined-to-group`)
-      break
-    default:
-      console.error(`Unknown action: ${action}`)
-  }
-  return result
-}
+const handleQuery = handleJoinAction
 
 export const register = (bot: Bot<BotContext>) => {
   // noinspection TypeScriptValidateTypes
