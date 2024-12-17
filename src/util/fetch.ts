e552ae7b8741fa983855c2bb43b9a6c7dc3dfab6
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-12-17 22:37:02 +0530

    add fetch with tiemout

---
+++ src/util/fetch.ts
@@ -0,0 +1,25 @@
+export const fetchJson = async <T>(
+  url: string,
+  options: RequestInit = {},
+  timeout = 1000,
+): Promise<T | undefined> => {
+  const controller = new AbortController() // Create an AbortController instance
+  const id = setTimeout(() => controller.abort(), timeout) // Set the timeout to abort the request
+  options.signal = controller.signal // Attach the signal to the request options
+
+  try {
+    const response = await fetch(url, options) // Send the fetch request
+    if (!response.ok) {
+      console.warn(`HTTP error! Status: ${response.status} on url: ${url}`)
+      return undefined
+    }
+    return await response.json() // Parse and return JSON response
+  } catch (error) {
+    if (error.name === 'AbortError') {
+      throw new Error('Request timed out')
+    }
+    throw error
+  } finally {
+    clearTimeout(id) // Clear the timeout when the request completes or fails
+  }
+}
