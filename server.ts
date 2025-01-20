fbaad1dcda4bb133f1e01d799e9c5c73a518db4d
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-01-20 20:30:52 +0530

    set deploy url only for production

--- server.ts
+++ server.ts
@@ -8,6 +8,9 @@ const handleUpdate: (...args: Request[]) => Promise<Response> = webhookCallback(
 
 const projectId = Deno.env.get(`DENO_PROJECT_ID`) || `telegram-saunabot`
 const deploymentId = Deno.env.get(`DENO_DEPLOYMENT_ID`)
+
+const IS_PRODUCTION = Boolean(Deno.env.get(`DENO_DEPLOYMENT_ID`))
+
 const deployUrl = deploymentId
   ? `https://${projectId}${deploymentId ? `-${deploymentId}` : ``}.deno.dev`
   : `http://localhost:8000`
@@ -47,8 +50,12 @@ Deno.serve(async (req) => {
 
 console.log(`Deno deploy url: ${deployUrl}`)
 
-// 5. Set webhook only for production
-await bot.api.setWebhook(`${deployUrl}/${bot.token}`, DEFAULT_CONFIG)
+if (IS_PRODUCTION) {
+  // 5. Set webhook only for production
+  await bot.api.setWebhook(`${deployUrl}/${bot.token}`, DEFAULT_CONFIG)
+
+  console.log(`Webhook is set to production url: ${deployUrl}`)
+}
 
 await bot.init()
 
