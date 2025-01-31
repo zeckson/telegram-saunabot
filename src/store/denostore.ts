cd3bad11fecd25b6d4b35602ec42335f3b737a4d
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-01-31 20:48:44 +0530

    add denostore

---
+++ src/store/denostore.ts
@@ -0,0 +1,37 @@
+import KvEntry = Deno.KvEntry
+import KvEntryMaybe = Deno.KvEntryMaybe
+import KvKey = Deno.KvKey
+import KvListSelector = Deno.KvListSelector
+
+const DENO_KV_URL = Deno.env.get('DENO_KV_URL')
+
+const openStore = () => {
+	if (DENO_KV_URL) return Deno.openKv(DENO_KV_URL)
+	else return Deno.openKv()
+}
+
+export class DenoStore {
+	constructor(private db: Deno.Kv) {
+	}
+
+	async save(key: KvKey, data: object): Promise<boolean> {
+		const result = await this.db.set(key, data)
+		return result.ok
+	}
+
+	async load(key: KvKey): Promise<KvEntryMaybe<object>> {
+		return await this.db.get(key)
+	}
+
+	async list(selector: KvListSelector): Promise<KvEntry<object>[]> {
+		return await Array.fromAsync(this.db.list(selector))
+	}
+
+	close() {
+		this.db.close()
+	}
+
+	static async create(): Promise<DenoStore> {
+		return new DenoStore(await openStore())
+	}
+}
