002d37ffe8e33549a63dad983a52e36805748bdc
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-02-21 20:04:16 +0530

    add access-store.ts

---
+++ src/store/access-store.ts
@@ -0,0 +1,20 @@
+import { Chat, User } from "../deps.ts"
+import { DenoStore } from "./denostore.ts"
+
+export class AccessStore {
+  constructor(private readonly store: DenoStore) {
+  }
+
+  async request(user: User, chat: Chat): Promise<boolean> {
+    return await this.store.save([`request`, user.id, `chat`, chat.id], chat)
+  }
+
+  async approve(user: User, chat: Chat): Promise<boolean> {
+    return await this.store.save([`approve`, user.id, `chat`, chat.id], chat)
+  }
+
+  async reject(user: User, chat: Chat): Promise<boolean> {
+    return await this.store.save([`reject`, user.id, `chat`, chat.id], chat)
+  }
+
+}
