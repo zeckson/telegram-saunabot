4e7c6022ea339c291a1066f03b984550cf18ab8e
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-06-10 23:29:07 +0300

    remove unused code

--- src/util/link.ts
+++ src/util/link.ts
@@ -1,10 +1,4 @@
 import { Chat, link } from '../deps.ts'
-import { chatLink, text } from './markdown.ts'
-
-export const getChatLink = (chat: Chat) => {
-	const title = chat.title ?? chat.type
-	return chat.username ? chatLink(title, chat.username) : text(title)
-}
 
 export const getFormattedChatLink = (chat: Chat) => {
 	const title = chat.title ?? chat.type
