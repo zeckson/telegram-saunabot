f59709fb66f32efab098680d6ed5210bc67d85e7
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-12-27 16:33:02 +0700

    cleanup

--- src/handler/user-command-composer.ts
+++ src/handler/user-command-composer.ts
@@ -1,13 +1,5 @@
 import { Composer } from 'grammy'
-import { Messages } from '../action/admin.messages.ts'
-import { declineUserJoinRequest, validateJoinRequest } from '../action/admin.ts'
-import { getBanInfo } from '../action/ban.ts'
-import { requestUserContact } from '../action/user.ts'
-import { ChatJoinRequest } from '../deps.ts'
 import { BotContext } from '../type/context.ts'
-import { User } from '../type/user.type.ts'
-import { link, text } from '../util/markdown.ts'
-import { int } from '../util/system.ts'
 
 const bot = new Composer<BotContext>()
 
