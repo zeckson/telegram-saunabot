017c53fbb465cb3e33ec4eb20632fb01c2b6ace2
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-12-27 20:33:32 +0700

    anonymous -> named

--- src/usecase/branch.ts
+++ src/usecase/branch.ts
@@ -1,13 +1,13 @@
 import { pipeline } from './pipeline.ts'
-import { Step } from "./sequence.type.ts"
+import { Step } from './sequence.type.ts'
 
 export const branch = <TCtx>(
 	predicate: (ctx: TCtx) => boolean | Promise<boolean>,
 	ifTrue: Step<TCtx>[],
 	ifFalse: Step<TCtx>[],
 ): Step<TCtx> =>
-async (ctx, name) => {
-	const ok = await predicate(ctx)
-	const next = pipeline(`${name}:branch:${ok}`, ok ? ifTrue : ifFalse)
-	return next(ctx)
-}
+	async function branch(ctx, name) {
+		const ok = await predicate(ctx)
+		const next = pipeline(`${name}:branch:${ok}`, ok ? ifTrue : ifFalse)
+		return next(ctx)
+	}
--- src/usecase/when.ts
+++ src/usecase/when.ts
@@ -5,17 +5,19 @@ export const other = Symbol(`other`)
 
 type TKey = string
 
-type Cases<TCtx, T extends TKey> = {
-	[key in T]?: Step<TCtx>[]
-} & { [other]: Step<TCtx>[] }
+type Cases<TCtx, T extends TKey> =
+	& {
+		[key in T]?: Step<TCtx>[]
+	}
+	& { [other]: Step<TCtx>[] }
 
 export const when = <TCtx, T extends TKey>(
 	getter: (ctx: TCtx) => T | Promise<T>,
 	cases: Cases<TCtx, T>,
 ): Step<TCtx> =>
-async (ctx, name) => {
-	const key = await getter(ctx)
-	const branch = cases[key] ?? cases[other]
-	const next = pipeline(`${name}:when:${key}`, branch)
-	return next(ctx)
-}
+	async function when(ctx, name) {
+		const key = await getter(ctx)
+		const branch = cases[key] ?? cases[other]
+		const next = pipeline(`${name}:when:${key}`, branch)
+		return next(ctx)
+	}
