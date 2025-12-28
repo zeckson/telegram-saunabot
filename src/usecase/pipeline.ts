a31b48a53183087867305619bef4ae72ade74e0d
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-12-28 13:30:47 +0700

    refactor fix

--- src/usecase/pipeline.ts
+++ src/usecase/pipeline.ts
@@ -1,11 +1,11 @@
-import { Sequence, Step, StepOutcome } from './sequence.type.ts'
+import { Step, StepOutcome } from './sequence.type.ts'
 
 const run = async <TCtx>(
-  sequence: Sequence<TCtx>,
+	sequence: { name: string; steps: Step<TCtx>[] },
 	ctx: TCtx,
 ): Promise<StepOutcome> => {
-  const name = sequence.name
-  const steps = sequence.steps
+	const name = sequence.name
+	const steps = sequence.steps
 
 	console.debug(`${name} ${steps.length} steps`)
 	for (let i = 0; i < steps.length; i++) {
@@ -14,21 +14,21 @@ const run = async <TCtx>(
 		const stepid = `${i + 1}/${steps.length}`
 		console.debug(`${stepName} executing step ${stepid} `)
 		try {
-      const res = await step(ctx, name)
-      if (!res.ok) {
-        console.warn(
-          `${stepName} returned "false" with reason: ${
-            res.reason ?? 'unknown reason'
-          }`,
-        )
-        return res
-      }
-      console.debug(`${stepName} completed successfully`)
-    } catch (e) {
-      const message = e instanceof Error ? e.message : String(e)
-      console.error(`${stepName} failed with error: ${message}`)
-      return { ok: false, reason: message } as const
-    }
+			const res = await step(ctx, name)
+			if (!res.ok) {
+				console.warn(
+					`${stepName} returned "false" with reason: ${
+						res.reason ?? 'unknown reason'
+					}`,
+				)
+				return res
+			}
+			console.debug(`${stepName} completed successfully`)
+		} catch (e) {
+			const message = e instanceof Error ? e.message : String(e)
+			console.error(`${stepName} failed with error: ${message}`)
+			return { ok: false, reason: message } as const
+		}
 	}
 	console.debug(`${name} completed successfully`)
 	return { ok: true } as const
--- src/usecase/sequence.type.ts
+++ src/usecase/sequence.type.ts
@@ -2,11 +2,8 @@ export type Reason = string | Error
 export type StepOutcome = { ok: true } | { ok: false; reason?: Reason }
 
 export type Step<TCtx> = (
-  ctx: TCtx,
-  name?: string,
+	ctx: TCtx,
+	parentName?: string,
 ) => Promise<StepOutcome> | StepOutcome
 
-export type Sequence<TCtx> = {
-  name: string,
-  steps: Step<TCtx>[]
-}
+export type Sequence<TCtx> = Step<TCtx> | Step<TCtx>[]
