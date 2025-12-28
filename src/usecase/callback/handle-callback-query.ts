cf93661e0f3b0ccb837ae9850cedaa16b8d6a80d
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-12-28 13:57:10 +0700

    refactor

--- src/usecase/callback/handle-callback-query.ts
+++ src/usecase/callback/handle-callback-query.ts
@@ -1,24 +1,30 @@
-import { JoinRequestAction } from "../../action/admin.ts"
+import { JoinRequestAction } from '../../action/admin.ts'
 import { pipeline } from '../pipeline.ts'
-import { other, when } from "../when.ts"
-import { answerCallbackQueryStep } from "./steps/answer-callback-query-step.ts"
+import { other, when } from '../when.ts'
 import { CallbackContextFlow } from './callback-context.type.ts'
+import { answerCallbackQueryStep } from './steps/answer-callback-query-step.ts'
 import { extractActionStep } from './steps/extract-action-step.ts'
-import { handleJoinRequestStep } from './steps/handle-join-request-step.ts'
-import { notifyAdminsResultStep } from './steps/notify-admins-result-step.ts'
+import { handleJoinRequestApproveStep, handleJoinRequestDeclineStep, } from './steps/handle-join-request-step.ts'
+import { notifyAdminsApprovedStep, notifyAdminsRejectedStep, } from './steps/notify-admins-result-step.ts'
 
 const fail = (reason: string) => () => ({ ok: false, reason })
 
 const handleCallbackQueryData = pipeline(`callback-data`, [
 	extractActionStep,
-  when(
-    (ctx: CallbackContextFlow) => ctx.data.action,
-    {
-      [JoinRequestAction.APPROVE]: [handleJoinRequestStep, notifyAdminsResultStep],
-      [JoinRequestAction.DECLINE]: [handleJoinRequestStep, notifyAdminsResultStep],
-      [other]: [fail(`unknown_action`)],
-    }
-  ),
+	when(
+		(ctx: CallbackContextFlow) => ctx.data.action,
+		{
+			[JoinRequestAction.APPROVE]: [
+				handleJoinRequestApproveStep,
+				notifyAdminsApprovedStep,
+			],
+			[JoinRequestAction.DECLINE]: [
+				handleJoinRequestDeclineStep,
+				notifyAdminsRejectedStep,
+			],
+			[other]: [fail(`unknown_action`)],
+		},
+	),
 ])
 
 export const handleCallbackQuery = async (ctx: CallbackContextFlow) =>
--- src/usecase/callback/steps/notify-admins-result-step.ts
+++ src/usecase/callback/steps/notify-admins-result-step.ts
@@ -1,25 +1,20 @@
-import { Messages } from "../../../action/admin.messages.ts"
-import { JoinRequestAction, notifyAllAdmins } from '../../../action/admin.ts'
-import { StepOutcome } from "../../sequence.type.ts"
+import { Messages } from '../../../action/admin.messages.ts'
+import { notifyAllAdmins } from '../../../action/admin.ts'
+import { StepOutcome } from '../../sequence.type.ts'
 import { CallbackContextFlow } from '../callback-context.type.ts'
 
-export const notifyAdminsResultStep = async (
+export const notifyAdminsApprovedStep = async (
 	ctx: CallbackContextFlow,
 ): Promise<StepOutcome> => {
-	const action = ctx.data.action
 	const userId = ctx.data.userId
-	try {
-		switch (action) {
-			case JoinRequestAction.APPROVE:
-				await notifyAllAdmins(ctx, Messages.notifyJoinApproved(ctx, userId))
-				return { ok: true }
-			case JoinRequestAction.DECLINE:
-        await notifyAllAdmins(ctx, Messages.notifyJoinRejected(ctx, userId))
-				return { ok: true }
-			default:
-				return { ok: false, reason: 'unknown_action' }
-		}
-	} catch (e: Error | unknown) {
-		return { ok: false, reason: e as Error }
-	}
+	await notifyAllAdmins(ctx, Messages.notifyJoinApproved(ctx, userId))
+	return { ok: true }
+}
+
+export const notifyAdminsRejectedStep = async (
+	ctx: CallbackContextFlow,
+): Promise<StepOutcome> => {
+	const userId = ctx.data.userId
+	await notifyAllAdmins(ctx, Messages.notifyJoinRejected(ctx, userId))
+	return { ok: true }
 }
