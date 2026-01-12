28efaf765f9478218afc206c23845f57805dc98b
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2026-01-12 20:11:23 +0700

    extract messages

---
+++ src/usecase/join/join.messages.ts
@@ -0,0 +1,36 @@
+import { bold, Chat, fmt, FormattedString, italic, link, } from '../../deps.ts'
+import { chatLink } from '../../text/chat.ts'
+import { userLink } from '../../text/user.ts'
+import { User } from '../../type/user.type.ts'
+import hashtag from '../../util/hashtag.ts'
+import { text } from '../../util/markdown.ts'
+
+const privacyPolicy = `https://snezhdanov.ru/privacy-policy`
+
+export const shareContactButtonName = 'Отправить контакт'
+
+export const chatJoinVerifyMessage = (
+	from: User,
+	chat: Chat,
+): FormattedString =>
+	fmt`Здравствуйте, ${bold(text(from.fullName))}!
+    
+Вы подали заявку на вступление в чат ${chatLink(chat)}.
+В целях борьбы со спамом и спам-аккаунтами мы просим вас поделиться с нами вашими контактными данными.
+Чтобы поделиться контактом нажмите кнопку ${bold(shareContactButtonName)}.
+  
+${
+		italic(`Мы не храним и не предоставляем данные третьим лицам. 
+Подробнее с политикой обработки персональных данных вы можете ознакомиться по `)
+	}${link(`ссылке`, privacyPolicy)} 
+    
+${bold(`Благодарим за понимание!`)}
+`
+
+export const requestContactError = (
+	user: User,
+	errorMessage: string,
+): FormattedString =>
+	fmt`Не удалось отправись запрос пользователю ${hashtag(user.id)}
+Запрос от ${userLink(user)}. Текст ошибки:
+  ${errorMessage}`
--- src/usecase/join/steps/request-user-contact-step.ts
+++ src/usecase/join/steps/request-user-contact-step.ts
@@ -1,19 +1,32 @@
+import { notifyAllAdmins } from "../../../action/admin.ts"
+import { GrammyError, Keyboard } from "../../../deps.ts"
 import { Step } from "../../sequence.type.ts"
 import type { JoinFlowContext } from "../join-context.ts"
-import { Messages } from "../../../action/admin.messages.ts"
-import { notifyAllAdmins } from "../../../action/admin.ts"
-import { requestUserContact } from "../../../action/user.ts"
-import type { GrammyError } from "../../../deps.ts"
+import { chatJoinVerifyMessage, requestContactError, shareContactButtonName } from "../join.messages.ts"
 
 export const requestUserContactStep: Step<JoinFlowContext> = async (ctx) => {
 	try {
-		await requestUserContact(ctx)
+		const message = chatJoinVerifyMessage(ctx.user, ctx.chat)
+		await ctx.api.sendMessage(
+			ctx.user_chat_id ?? ctx.from.id,
+			message.text,
+			{
+				link_preview_options: { is_disabled: true },
+				entities: message.entities,
+				reply_markup: {
+					keyboard: [[
+						Keyboard.requestContact(shareContactButtonName),
+					]],
+					is_persistent: true,
+				},
+			},
+		)
 		return { ok: true }
 	} catch (e: unknown) {
 		await notifyAllAdmins(
 			ctx,
-			Messages.requestContactError(ctx, e as GrammyError),
+			requestContactError(ctx.user, (e as GrammyError).message),
 		)
-		return { ok: false, reason: "request_contact_failed" }
+		return { ok: false, reason: 'request_contact_failed' }
 	}
 }
