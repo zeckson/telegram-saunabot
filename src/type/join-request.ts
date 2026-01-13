c814424661431c34d21b51864bf0af8b96bb6271
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2026-01-13 07:27:06 +0700

    add join-request.ts type

---
+++ src/type/join-request.ts
@@ -0,0 +1,10 @@
+export const enum JoinRequestAction {
+  APPROVE = `approve`,
+  DECLINE = `decline`,
+}
+
+export interface JoinRequestData {
+  action: JoinRequestAction
+  userId: number
+  chatId: number
+}
